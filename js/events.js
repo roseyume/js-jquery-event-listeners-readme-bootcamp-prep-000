a205d969ed33e2963879687272d942171b519101
Rosie Wang <rosiedwang@yahoo.com>
2018-02-09 21:53:27 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,10 @@
 //define functions here
+function getIt(){
+  $(p).on('click',alert("Hey!"));
+}
 
 $(document).ready(function(){
 
 // call functions here
-
+  $('p').on('click',alert("Hey!"));
 });
