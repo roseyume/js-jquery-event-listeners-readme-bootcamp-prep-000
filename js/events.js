49c4eedce3d52c7f35a1f648cc2e42726ca0544e
Rosie Wang <rosiedwang@yahoo.com>
2018-02-13 21:15:16 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,10 +1,31 @@
 //define functions here
 function getIt(){
-  $(p).on('click',alert("Hey!"));
+  $('p').on('click',alert("Hey!"));
 }
 
+function frameIt(){
+  /*$("img").on('load',);*/
+  $("img").addClass("tasty");
+}
+
+function submitIt()
+{
+  $('form').on('submit',alert('Your form is going to be submitted now.'));
+}
+
+function pressIt()
+{
+  $('form').on('keydown',function(){
+    if($("input:first")==="G"){
+      alert('You pressed G');
+    }
+  })
+}
 $(document).ready(function(){
 
 // call functions here
-  $('p').on('click',alert("Hey!"));
+ getIt();
+ frameIt();
+ submitIt();
+ pressIt();
 });
